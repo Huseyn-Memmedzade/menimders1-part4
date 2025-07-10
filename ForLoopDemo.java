60e6dfb1ef634b48f94de9f18d604ac91f85c329
Backend Developer <hseyn.mmmdzad@bk.ru>
2025-07-10 16:08:06 +0400

    Add files via upload

---
+++ ForLoopDemo.java
@@ -0,0 +1,16 @@
+package yenipabka;
+
+public class ForLoopDemo {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 10; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
+
+// Tapşırıq 5: For Dövrü ilə Təcrübə
+// ForLoopDemo adlı bir sinif yaradın.
+// 1-dən 10-a qədər olan ədədləri çap edən bir for dövrü yazın.
+// Dövrü elə dəyişdirin ki, yalnız cüt ədədləri çap etsin.
+// Tapsiriqlar Yerine Yetirildi✅
